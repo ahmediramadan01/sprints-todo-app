8717ff9c6cdb93a62e35b9943f200e33aa45deb8
Ahmed Ramadan <ahmediramadan01@proton.me>
2023-05-30 19:50:54 +0300

    Implement add and remove tasks

---
+++ script.js
@@ -0,0 +1,53 @@
+"use strict";
+
+const taskName = document.querySelector(".name");
+const taskPriority = document.querySelector("#priority");
+
+const btnAdd = document.querySelector(".add");
+const btnsRemove = document.querySelectorAll(".remove");
+
+const table = document.querySelector(".table");
+const tableBody = document.querySelector(".table-body");
+
+let tasks = [];
+
+const createTable = function () {
+  tableBody.innerHTML = "";
+  for (let i = 0; i < tasks.length; i++) {
+    tableBody.innerHTML += `
+    <tr">
+        <td>${i + 1}</td>
+        <td>${tasks[i][0]}</td>
+        <td>${tasks[i][1]}</td>
+        <td><button id="${i}" class="remove" onclick="removeTask(${i})">Remove</button></td>
+    </tr>
+    `;
+  }
+};
+
+const addTask = function () {
+  if (taskName.value === "") {
+    alert("Please enter a Task");
+  } else {
+    if (tasks.length === 0) {
+      table.classList.remove("hidden");
+    }
+    tasks.push([taskName.value, taskPriority.value]);
+    createTable();
+  }
+};
+
+function removeTask(x) {
+  tasks.splice(x, 1);
+  createTable();
+  if (tasks.length === 0) {
+    table.classList.add("hidden");
+  }
+}
+
+btnAdd.addEventListener("click", addTask);
+document.addEventListener("keydown", function (event) {
+  if (event.key === "Enter") {
+    addTask();
+  }
+});
