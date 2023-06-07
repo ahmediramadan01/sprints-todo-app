319e2542f22476ec4adc792f31fd6618d033bd00
Ahmed Ramadan <ahmediramadan01@proton.me>
2023-06-07 23:11:04 +0300

    Convert code to OOP

--- script.js
+++ script.js
@@ -1,120 +1,123 @@
 "use strict";
 
-const taskName = document.querySelector(".task-name");
-const taskPriority = document.querySelector("#task-priority");
+class Task {
+  constructor() {
+    this.taskName = document.querySelector(".task-name");
+    this.taskPriority = document.querySelector("#task-priority");
 
-const addBtn = document.querySelector(".add");
-const sortBtn = document.querySelector(".sort");
+    this.addBtn = document.querySelector(".add");
+    this.sortBtn = document.querySelector(".sort");
 
-const table = document.querySelector(".table");
-const tableBody = document.querySelector(".table-body");
+    this.table = document.querySelector(".table");
+    this.tableBody = document.querySelector(".table-body");
 
-const modal = document.querySelector(".modal");
-const overlay = document.querySelector(".overlay");
-const closeModalBtn = document.querySelector(".close-modal");
+    this.modal = document.querySelector(".modal");
+    this.overlay = document.querySelector(".overlay");
+    this.closeModalBtn = document.querySelector(".close-modal");
 
-let tasks = [];
-let sortingPriority = ["High", "Medium", "Low"];
+    this.tasks = [];
+    this.sortingPriority = ["High", "Medium", "Low"];
 
-const renderTable = function () {
-  tableBody.innerHTML = "";
-  for (let i = 0; i < tasks.length; i++) {
-    tableBody.innerHTML += `
-    <tr">
-        <td>${i + 1}</td>
-        <td>${
-          tasks[i][2]
-            ? `<input id="edit-task-${i}" class="edit-task" type="text" value="${tasks[i][0]}">`
-            : tasks[i][0]
-        }</td>
-        <td>${tasks[i][2] ? getSelect(i) : tasks[i][1]}</td>
-        <td>
-          <button data-id="${i}" id="edit-${i}" class="edit" onclick="editTask(${i})">Edit</button>
-          <button data-id="${i}" id="save-${i}" class="save hidden" onclick="saveTask(${i})">Save</button>
-          <button data-id="${i}" id="remove-${i}" class="remove" onclick="removeTask(${i})">Remove</button>
-        </td>
-    </tr>
-    `;
+    this.addBtn.addEventListener("click", this.addTask.bind(this));
+    this.sortBtn.addEventListener("click", this.sortTasks.bind(this));
+
+    this.closeModalBtn.addEventListener("click", this.closeModal.bind(this));
+    this.overlay.addEventListener("click", this.closeModal.bind(this));
+    document.addEventListener("keydown", (event) => {
+      if (event.key === "Escape" && !this.modal.classList.contains("hidden")) {
+        this.closeModal();
+      }
+    });
   }
-};
 
-const getSelect = function (i) {
-  return `<select name="priority" class="edit-priority" id="edit-priority-${i}">
-  <option value="High" id="high" ${
-    tasks[i][1] == "High" ? "selected" : ""
-  }>High</option>
-  <option value="Medium" id="medium" ${
-    tasks[i][1] == "Medium" ? "selected" : ""
-  }>Medium</option>
-  <option value="Low" id="low" ${
-    tasks[i][1] == "Low" ? "selected" : ""
-  }>Low</option>
-</select>`;
-};
-
-const addTask = function () {
-  if (taskName.value.trim() === "") {
-    openModal();
-  } else {
-    if (tasks.length === 0) {
-      table.classList.remove("hidden");
+  renderTable() {
+    this.tableBody.innerHTML = "";
+    for (let i = 0; i < this.tasks.length; i++) {
+      this.tableBody.innerHTML += `
+      <tr">
+          <td>${i + 1}</td>
+          <td>${
+            this.tasks[i][2]
+              ? `<input id="edit-task-${i}" class="edit-task" type="text" value="${this.tasks[i][0]}">`
+              : this.tasks[i][0]
+          }</td>
+          <td>${this.tasks[i][2] ? this.getSelect(i) : this.tasks[i][1]}</td>
+          <td>
+            <button data-id="${i}" id="edit-${i}" class="edit" onclick="task.editTask(${i})">Edit</button>
+            <button data-id="${i}" id="save-${i}" class="save hidden" onclick="task.saveTask(${i})">Save</button>
+            <button data-id="${i}" id="remove-${i}" class="remove" onclick="task.removeTask(${i})">Remove</button>
+          </td>
+      </tr>
+      `;
     }
-    tasks.push([taskName.value, taskPriority.value, false]);
-    renderTable(tasks);
-    taskName.value = "";
   }
-};
 
-const removeTask = function (i) {
-  tasks.splice(i, 1);
-  renderTable(tasks);
-  if (tasks.length === 0) {
-    table.classList.add("hidden");
+  getSelect(i) {
+    return `<select name="priority" class="edit-priority" id="edit-priority-${i}">
+    <option value="High" id="high" ${
+      this.tasks[i][1] == "High" ? "selected" : ""
+    }>High</option>
+    <option value="Medium" id="medium" ${
+      this.tasks[i][1] == "Medium" ? "selected" : ""
+    }>Medium</option>
+    <option value="Low" id="low" ${
+      this.tasks[i][1] == "Low" ? "selected" : ""
+    }>Low</option>
+  </select>`;
   }
-};
 
-const currentTask = function (i) {
-  return tasks[i];
-};
-
-const editTask = function (i) {
-  tasks[i][2] = true;
-  renderTable(tasks);
-  document.querySelector(`#save-${i}`).classList.remove("hidden");
-  document.querySelector(`#edit-${i}`).classList.add("hidden");
-};
-
-const saveTask = function (i) {
-  tasks[i][0] = document.querySelector(`#edit-task-${i}`).value;
-  tasks[i][1] = document.querySelector(`#edit-priority-${i}`).value;
-  tasks[i][2] = false;
-  renderTable(tasks);
-};
-
-const sortTasks = function () {
-  tasks.sort(
-    (a, b) => sortingPriority.indexOf(a[1]) - sortingPriority.indexOf(b[1])
-  );
-  renderTable();
-};
-
-const openModal = function () {
-  modal.classList.remove("hidden");
-  overlay.classList.remove("hidden");
-};
-
-const closeModal = function () {
-  modal.classList.add("hidden");
-  overlay.classList.add("hidden");
-};
-
-addBtn.addEventListener("click", addTask);
-sortBtn.addEventListener("click", sortTasks);
-
-closeModalBtn.addEventListener("click", closeModal);
-overlay.addEventListener("click", closeModal);
-document.addEventListener("keydown", function (event) {
-  if (event.key === "Escape" && !modal.classList.contains("hidden")) {
-    closeModal();
+  addTask() {
+    if (this.taskName.value.trim() === "") {
+      this.openModal();
+    } else {
+      if (this.tasks.length === 0) {
+        this.table.classList.remove("hidden");
+      }
+      this.tasks.push([this.taskName.value, this.taskPriority.value, false]);
+      this.renderTable();
+      this.taskName.value = "";
+    }
   }
-});
+
+  removeTask(i) {
+    this.tasks.splice(i, 1);
+    this.renderTable();
+    if (this.tasks.length === 0) {
+      this.table.classList.add("hidden");
+    }
+  }
+
+  editTask(i) {
+    this.tasks[i][2] = true;
+    this.renderTable();
+    document.querySelector(`#save-${i}`).classList.remove("hidden");
+    document.querySelector(`#edit-${i}`).classList.add("hidden");
+  }
+
+  saveTask(i) {
+    this.tasks[i][0] = document.querySelector(`#edit-task-${i}`).value;
+    this.tasks[i][1] = document.querySelector(`#edit-priority-${i}`).value;
+    this.tasks[i][2] = false;
+    this.renderTable();
+  }
+
+  sortTasks() {
+    this.tasks.sort(
+      (a, b) =>
+        this.sortingPriority.indexOf(a[1]) - this.sortingPriority.indexOf(b[1])
+    );
+    this.renderTable();
+  }
+
+  openModal() {
+    this.modal.classList.remove("hidden");
+    this.overlay.classList.remove("hidden");
+  }
+
+  closeModal() {
+    this.modal.classList.add("hidden");
+    this.overlay.classList.add("hidden");
+  }
+}
+
+const task = new Task();
